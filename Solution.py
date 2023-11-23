d1f21a529d0289a4b6573023eb957aa573df2b22
Welberth Azevedo Barbosa <welberthab@gmail.com>
2023-11-22 21:21:35 -0300

    Solution

---
+++ Solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        d = {}
+        ans = []
+
+        for i in nums2:
+            while stack and stack[-1] < i:
+                d[stack.pop()] = i
+            stack.append(i)
+
+        for i in nums1:
+            ans.append(d.get(i, -1))
+        return ans
